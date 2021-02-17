d4eb30397b40162f1de25938a32143c3ee2bc5b2
George Vlahavas <vlahavas@gmail.com>
2021-02-17 10:14:43 +0200

    Add sleep.sh script
    Useful for starting the container without actually doing anything, but
    staying on nevertheless.

---
+++ sleep.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Used for starting the docker container, without it actually doing
+# anything. Just waiting for when to start the benchmarks once
+# everything is setup.
+
+while true; do
+  sleep 10
+done
